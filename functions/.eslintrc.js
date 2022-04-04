f5f19459ceeb73f53456c8b1eec2c41fbb45568e
Henry Chan <henry@kinwo.net>
2022-04-04 15:14:01 +1000

    [Content Fly Cloud] Init commit
    * Github Action CD
    * Hello world cloud function

---
+++ functions/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'object-curly-spacing': 0,
+  },
+};
