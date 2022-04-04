ab6c1d10e3bf9b322d61be987ab9641b291152f4
Henry Chan <henry@kinwo.net>
2022-04-04 16:57:27 +1000

    [Lint] Add Husky pre commit Lint
    * setup VSCode, Prettier

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,10 +2,14 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
-  extends: ['eslint:recommended', 'google'],
+  extends: ["eslint:recommended", "google"],
   rules: {
-    'object-curly-spacing': 0,
-  },
-};
+    "object-curly-spacing": 0,
+    "comma-dangle": 0,
+    "quote-props": 0,
+    semi: 0,
+    quotes: 0
+  }
+}
