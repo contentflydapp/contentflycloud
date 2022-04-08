8b588fb4ccbfb6c61657da258d20c7413de08b4a
Henry Chan <henry@kinwo.net>
2022-04-08 15:34:24 +1000

    [FetchEmailNotification] Update schedule job to run every 2 mins

--- functions/entry/email.js
+++ functions/entry/email.js
@@ -39,7 +39,7 @@ const axiosInstance = axios.create({
  */
 exports.fetchNewNotifications = functions
   .runWith(runtimeOpts)
-  .pubsub.schedule("every 120 minutes")
+  .pubsub.schedule("every 2 minutes")
   .onRun(async context => {
     try {
       const notifyQueueURL = process.env.NOTIFY_QUEUE_URL
